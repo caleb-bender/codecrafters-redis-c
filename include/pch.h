15d1c82789586ad1f7685aaa2661731acd0982c1
Caleb Bender <cbender@dynamisoft.net>
2026-02-11 22:29:54 -0800

    Project setup

---
+++ include/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#endif //PCH_H
